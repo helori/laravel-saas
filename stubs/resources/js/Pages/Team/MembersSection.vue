2f5100487483a7850caffd91e6ddfb16e982e0d0
helori <helori.lanos@gmail.com>
2022-04-26 15:20:51 +0200

    Members login UI

--- stubs/resources/js/Pages/Team/MembersSection.vue
+++ stubs/resources/js/Pages/Team/MembersSection.vue
@@ -75,12 +75,22 @@
                                 <td class="text-right">
                                     <span class="btn btn-gray ml-2" 
                                         @click="openUpdate(item)">
-                                        <PencilAltIcon class="h-5 w-5"/>
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                        </svg>
                                     </span>
                                     <span class="btn btn-gray ml-2" 
                                         @click="openDestroy(item)">
-                                        <TrashIcon class="h-5 w-5"/>
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                        </svg>
                                     </span>
+                                    <a class="btn btn-gray ml-2" 
+                                        :href="'/team/' + user.current_team.id + '/member/' + item.id + '/login'">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                                        </svg>
+                                    </a>
                                 </td>
                             </tr>
                         </tbody>
